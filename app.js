3fdb0f25ee5380ae4fb359159cd4e200b72887ce
RileyManda <rileymanda69@gmail.com>
2023-05-29 20:07:02 +0200

    Added program background image

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const openMenu = function openMenuHandler() {
   document.querySelector('.mobile-menu').style.display = 'block';
 };
 
-const closeView = function closeNavHandler() {
-  document.querySelector('.mobile-menu').style.display = 'none';
+const closeView = function closeNavHandler(event) {
+  event.preventDefault(); // Prevent the default link behavior
+
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.style.display = 'none';
+
+  const clickedLink = event.target.closest('a');
+
+  // Check if the clicked link corresponds to the home section
+  if (clickedLink && clickedLink.getAttribute('href') === '#home-section') {
+    window.location.href = 'index.html';
+  }
 };
 
 document.querySelector('#open-menu').addEventListener('click', openMenu);
