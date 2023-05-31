6cea145390a1e2b14e393896644f52624e45fbd2
RileyManda <rileymanda69@gmail.com>
2023-05-31 04:25:44 +0200

    Bug fixes and updated content

--- api/data.js
+++ api/data.js
@@ -52,3 +52,50 @@ export const pastSummitsData = [
     alt: 'CC Global Summit 2011',
   },
 ];
+export const speakerData = [
+  {
+    image: './assets/images/speaker_01.svg',
+    name: 'John Wayne',
+    credential:
+      'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description:
+      'John Wayne is a renowned expert in entrepreneurial legal studies. With his extensive experience and expertise, he has contributed significantly to the field of law and entrepreneurship.',
+  },
+  {
+    image: './assets/images/speaker_02.svg',
+    name: 'Michael Pong',
+    credential: 'Director of CGI And Graphics at Universal Pictures',
+    description:
+      'Michael Pong is a visionary director in CGI and graphics. With his creative skills and innovative approach, he has played a pivotal role in shaping the visual effects industry.',
+  },
+  {
+    image: './assets/images/speaker_03.png',
+    name: 'Tasha Jonathan',
+    credential: 'Berkman Professor of Robotics Engineering Sciences at MIT',
+    description:
+      'Tasha Jonathan is a leading authority in robotics engineering sciences. Her groundbreaking research and contributions have revolutionized the field of robotics.',
+  },
+  {
+    image: './assets/images/speaker_04.png',
+    name: 'Angela Ping',
+    credential:
+      'Director of Artificial Intelligence and Robotics at Massachusetts',
+    description:
+      'Angela Ping is a prominent figure in the field of artificial intelligence and robotics. Her expertise and leadership have paved the way for advancements in AI and robotics technologies.',
+  },
+  {
+    image: './assets/images/speaker_05.png',
+    name: 'Mary King',
+    credential:
+      'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description:
+      'Mary King is a distinguished professor specializing in entrepreneurial legal studies. Her profound knowledge and research have contributed significantly to the understanding of legal aspects in entrepreneurship.',
+  },
+  {
+    image: './assets/images/speaker_06.png',
+    name: 'Arnold Prince',
+    credential: 'Director of Machine Learning at SapienAi',
+    description:
+      'Arnold Prince is a leading expert in machine learning. With his deep understanding of the field, he has made significant contributions to the advancement of machine learning algorithms and applications.',
+  },
+];
--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const openMenu = function openMenuHandler() {
+const openMenu = function openMenuHandler(event) {
+  event.preventDefault();
   document.querySelector('.mobile-menu').style.display = 'block';
 };
 
 const closeView = function closeNavHandler(event) {
+  event.preventDefault();
   const mobileMenu = document.querySelector('.mobile-menu');
   mobileMenu.style.display = 'none';
 
   const clickedLink = event.target.closest('a');
-
-  // Check if the clicked link corresponds to the home section
   if (clickedLink && clickedLink.getAttribute('href') === '#home-section') {
     window.location.href = 'index.html';
   }
 };
 
-document.querySelector('#open-menu').addEventListener('click', openMenu);
-document.querySelector('#close-view').addEventListener('click', closeView);
-
 // Add click event listener to the logo
-document.querySelector('.logo').addEventListener('click', () => {
+document.querySelector('.logo').addEventListener('click', (event) => {
+  event.preventDefault();
   window.location.href = 'index.html';
 });
+
+document.querySelector('#open-menu').addEventListener('click', openMenu);
+document.querySelector('#close-view').addEventListener('click', closeView);
