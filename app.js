360c13b1142821dfa0d3893bb23825ce0165cdde
RileyManda <rileymanda69@gmail.com>
2023-06-02 17:46:30 +0200

    Bug fix:Fixed the  Menu item open by default and menu not closing onclick menu items

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-const openMenu = function openMenuHandler(event) {
-  event.preventDefault();
-  document.querySelector('.mobile-menu').style.display = 'block';
+const openMenu = () => {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.style.display = 'block';
 };
 
-const closeView = function closeNavHandler(event) {
-  event.preventDefault();
+const closeMenu = () => {
   const mobileMenu = document.querySelector('.mobile-menu');
   mobileMenu.style.display = 'none';
-
-  const clickedLink = event.target.closest('a');
-  if (clickedLink && clickedLink.getAttribute('href') === '#home-section') {
-    window.location.href = 'index.html';
-  }
 };
 
-// Add click event listener to the logo
-document.querySelector('.logo').addEventListener('click', (event) => {
-  event.preventDefault();
-  window.location.href = 'index.html';
-});
-
 document.querySelector('#open-menu').addEventListener('click', openMenu);
-document.querySelector('#close-view').addEventListener('click', closeView);
+document.querySelector('#close-view').addEventListener('click', closeMenu);
+
+// Hide the mobile menu on page load
+window.addEventListener('load', () => {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.style.display = 'none';
+});
